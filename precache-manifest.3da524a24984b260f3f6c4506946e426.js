45c8889d36093d50a6ade66ce244d824f30d9867
kehinde <bankolek5@gmail.com>
2020-01-16 03:53:15 +0100

    Updates

---
+++ precache-manifest.3da524a24984b260f3f6c4506946e426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4cca6c2e8d8f26886c359dca565c92",
+    "url": "/reactadmin/index.html"
+  },
+  {
+    "revision": "18f428da46a5b73e5c76",
+    "url": "/reactadmin/static/css/main.05e7c8f1.chunk.css"
+  },
+  {
+    "revision": "5ecc4206197c9fd5a407",
+    "url": "/reactadmin/static/js/2.629d3e98.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/reactadmin/static/js/2.629d3e98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18f428da46a5b73e5c76",
+    "url": "/reactadmin/static/js/main.7ea1e5f8.chunk.js"
+  },
+  {
+    "revision": "f9185363ff11eb5b64cc",
+    "url": "/reactadmin/static/js/runtime-main.f6d4aee0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactadmin/static/media/logo.5d5d9eef.svg"
+  }
+]);
